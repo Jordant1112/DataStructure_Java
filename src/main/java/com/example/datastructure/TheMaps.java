4241954963c9e1c7c2ce97e6e412187f2eca5174
Jordant1112 <jordant1112@gmail.com>
2022-09-21 09:27:42 -0500

    Added HashMap in TheMaps

--- src/main/java/com/example/datastructure/TheMaps.java
+++ src/main/java/com/example/datastructure/TheMaps.java
@@ -2,9 +2,21 @@ package com.example.datastructure;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class TheMaps {
     public static void main(String[] args) {
+
+        Map<Person, Diamond> map = new HashMap<>();
+        map.put(new Person("Tom"), new Diamond("Africa Diamond"));
+        System.out.println(new Person("Tom").hashCode());
+        System.out.println(new Person("Tom").hashCode());
+        System.out.println(map.get(new Person("Tom")));
+        //Note: need to override hashCode in the Person Class
+        //Otherwise, the hashcode is different
+    }
+
+    private static void maps() {
         Map<Integer, Person> map = new HashMap<>();
         map.put(1, new Person("Tom"));
         map.put(2, new Person("charlie"));
@@ -17,5 +29,32 @@ public class TheMaps {
         System.out.println(map.entrySet());
         System.out.println(map.keySet());
     }
-    record Person(String name){}
+
+    static class Person{
+        String name;
+        public Person(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Person person = (Person) o;
+            return Objects.equals(name, person.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name);
+        }
+    }
+    record Diamond(String name){}
 }
